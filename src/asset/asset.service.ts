83a6812c4dfd807b8c13c161d8dc8f9ca9a4571a
okunladekayode <okunladekayode@gmail.com>
2022-10-21 17:06:16 +0100

    get all users

--- src/asset/asset.service.ts
+++ src/asset/asset.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
+import { Asset, AssetDocument } from './schemas/asset.schema';
 
 @Injectable()
 export class AssetService {
-  create(createAssetDto: CreateAssetDto) {
-    return 'This action adds a new asset';
+  constructor(
+    @InjectModel(Asset.name) private readonly assetModel: Model<AssetDocument>,
+  ) {}
+
+  async create(createAssetDto: CreateAssetDto) {
+    const { artId } = createAssetDto;
+    const asset = await this.assetModel.findOne({ artId });
+    if (asset) {
+      // throw new HttpException('user already exists', HttpStatus.BAD_REQUEST);
+      return asset;
+    }
+
+    const createdAsset = await new this.assetModel({
+      ...createAssetDto,
+      createdAt: new Date(),
+    }).save();
+
+    return createdAsset;
   }
 
   findAll() {
---
+++ src/asset/schemas/asset.schema.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type AssetDocument = Asset & Document;
+
+@Schema({ versionKey: false })
+export class Asset {
+  @Prop({ required: true })
+  artistName: string;
+
+  @Prop({ required: true, unique: true })
+  artId: string;
+
+  @Prop({ required: true })
+  artTitle: string;
+
+  @Prop({ required: true })
+  artSymbol: string;
+
+  @Prop({ required: true })
+  artDescription: string;
+
+  @Prop({ required: true })
+  artCreationYear: Date;
+
+  @Prop({ required: true })
+  issuerEmail: string;
+
+  @Prop()
+  artValue: string;
+
+  @Prop()
+  artPicture: string;
+
+  @Prop()
+  pricePerToken: number;
+
+  @Prop()
+  numberOftokens: number;
+
+  @Prop()
+  numberOfTokensForSale: string;
+
+  @Prop({ required: true })
+  createdAt: Date;
+
+  @Prop()
+  deletedAt?: Date;
+}
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
